0f8a4167974d31cb0db0a669f7e80245316c06b9
Jacob Hughes <97858392+OrangeCash090@users.noreply.github.com>
2024-08-30 10:15:04 -0400

    Update BedrockWS.js

--- modules/BedrockWS.js
+++ modules/BedrockWS.js
@@ -14,7 +14,6 @@ class Server {
 
         this.connectedSockets = [];
         this.lastId = 0;
-        this.vrTrackers = {};
 
         this.websocket.on("connection", async (conn) => {
             var playerName = `Guest ${this.lastId}`;
@@ -36,7 +35,7 @@ class Server {
                 var parsedMsg = JSON.parse(msg);
 
                 if (parsedMsg.head != undefined) {
-                    this.vrTrackers = parsedMsg;
+                    socket.emit("VRTrackingData", parsedMsg);
                 } else {
                     var reqID = parsedMsg.header.requestId;
                     var resolver = socket.responseResolvers.get(reqID);
