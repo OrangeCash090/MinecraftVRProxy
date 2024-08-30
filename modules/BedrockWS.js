6b56926983fcd98a504bb587fec119dffe8e7cd2
Jacob Hughes <97858392+OrangeCash090@users.noreply.github.com>
2024-08-30 10:03:19 -0400

    Update BedrockWS.js

--- modules/BedrockWS.js
+++ modules/BedrockWS.js
@@ -36,7 +36,6 @@ class Server {
                 var parsedMsg = JSON.parse(msg);
 
                 if (parsedMsg.head != undefined) {
-                    console.log(parsedMsg.head);
                     this.vrTrackers = parsedMsg;
                 } else {
                     var reqID = parsedMsg.header.requestId;
