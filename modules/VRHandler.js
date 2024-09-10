454c7f72a487a597f3b6ce7589122c98a0aba49d
Jacob Hughes <97858392+OrangeCash090@users.noreply.github.com>
2024-09-10 07:07:31 -0400

    Update VRHandler.js

--- modules/VRHandler.js
+++ modules/VRHandler.js
@@ -77,7 +77,28 @@ class VRHandler {
                     playerTransforms: playerTransforms
                 }));
             }
-        }, 150)
+        }, 150);
+
+        setTimeout(async () => {
+            if (!this.loadingWorld) {
+                var data = [];
+
+                this.loadingWorld = true;
+                this.trackingPlayers = false;
+                this.rendering = false;
+
+                await new Promise(resolve => setTimeout(resolve, 1000));
+                data = await JSONSender.getChunk(ws, this.headCube.cframe.position);
+
+                this.loadingWorld = false;
+                this.trackingPlayers = true;
+                this.rendering = true;
+
+                vrSocket.send(JSON.stringify({
+                    blockCoords: data
+                }));
+            }
+        }, 3000);
     }
 }
 
