73f73acc3055af2f7622cca9bd3fc0bf3b884cc2
Jacob Hughes <97858392+OrangeCash090@users.noreply.github.com>
2024-09-19 15:35:13 -0400

    Update VRHandler.js

--- modules/VRHandler.js
+++ modules/VRHandler.js
@@ -1,7 +1,6 @@
 const JSONSender = require("./JSONSender");
 const PlayerHandler = require("./PlayerHandler");
-const { CFrame, CFAngles } = require("./CFrame");
-const {Mesh, Cube} = require("./Mesh");
+const BlockEntity = require("./BlockEntity");
 const { Vec3 } = require("vec3");
 
 function toRadians(angle) {
@@ -16,16 +15,16 @@ class VRHandler {
     constructor (ws, vrSocket) {
         this.loadingWorld = false;
         this.trackingPlayers = true;
-        this.rendering = true;
+        this.rendering = false;
         this.pickedBlock = "stone";
 
-        this.headCube = new Cube(ws, 3);
-        this.leftCube = new Cube(ws, 1);
-        this.rightCube = new Cube(ws, 1);
-        
-        this.headCube.size = new Vec3(1, 1, 1);
-        this.leftCube.size = new Vec3(0.5, 0.5, 0.5);
-        this.rightCube.size = new Vec3(0.5, 0.5, 0.5);
+        this.headCube = new BlockEntity(ws, "gold_block", "head", new Vec3(0,0,0));
+        this.leftCube = new BlockEntity(ws, "gold_block", "left", new Vec3(0,0,0));
+        this.rightCube = new BlockEntity(ws, "gold_block", "right", new Vec3(0,0,0));
+
+        this.headCube.updating = true;
+        this.leftCube.updating = true;
+        this.rightCube.updating = true;
 
         vrSocket.on("VRTrackingData", (vrTrackers) => {
             if (this.rendering) {
@@ -36,10 +35,6 @@ class VRHandler {
                 this.headCube.cframe = new CFrame(head.position.x * 4, head.position.y * 4, head.position.z * 4).multiply(CFAngles(toRadians(head.rotation.x), toRadians(head.rotation.y), 0));
                 this.leftCube.cframe = new CFrame(leftHand.position.x * 4, leftHand.position.y * 4, leftHand.position.z * 4).multiply(CFAngles(toRadians(leftHand.rotation.x), toRadians(leftHand.rotation.y), 0));
                 this.rightCube.cframe = new CFrame(rightHand.position.x * 4, rightHand.position.y * 4, rightHand.position.z * 4).multiply(CFAngles(toRadians(rightHand.rotation.x), toRadians(rightHand.rotation.y), 0));
-    
-                this.headCube.update();
-                this.leftCube.update();
-                this.rightCube.update();
             }
         });
 
