46074a9629460fe495d832c50fc460571754b144
Jacob Hughes <97858392+OrangeCash090@users.noreply.github.com>
2024-09-21 09:44:17 -0400

    Update VRHandler.js

--- modules/VRHandler.js
+++ modules/VRHandler.js
@@ -1,7 +1,6 @@
 const JSONSender = require("./JSONSender");
 const PlayerHandler = require("./PlayerHandler");
-const { CFrame, CFAngles } = require("./CFrame");
-const {Mesh, Cube} = require("./Mesh");
+const BlockEntity = require("./BlockEntity");
 const { Vec3 } = require("vec3");
 
 function toRadians(angle) {
@@ -16,16 +15,26 @@ class VRHandler {
     constructor (ws, vrSocket) {
         this.loadingWorld = false;
         this.trackingPlayers = true;
-        this.rendering = true;
+        this.rendering = false;
         this.pickedBlock = "stone";
 
-        this.headCube = new Cube(ws, 3);
-        this.leftCube = new Cube(ws, 1);
-        this.rightCube = new Cube(ws, 1);
-        
-        this.headCube.size = new Vec3(1, 1, 1);
-        this.leftCube.size = new Vec3(0.5, 0.5, 0.5);
-        this.rightCube.size = new Vec3(0.5, 0.5, 0.5);
+        this.headCube;
+        this.leftCube;
+        this.rightCube;
+
+        this.makeBody = async () => {
+            this.headCube = await new BlockEntity(ws, "noteblock", "head", new Vec3(0,0,0), 1);
+            this.leftCube = await new BlockEntity(ws, "noteblock", "left", new Vec3(0,0,0), 0.5);
+            this.rightCube = await new BlockEntity(ws, "noteblock", "right", new Vec3(0,0,0), 0.5);
+
+            this.headCube.updating = true;
+            this.leftCube.updating = true;
+            this.rightCube.updating = true;
+
+            this.rendering = true;
+        }
+
+        this.makeBody();
 
         vrSocket.on("VRTrackingData", (vrTrackers) => {
             if (this.rendering) {
@@ -33,13 +42,14 @@ class VRHandler {
                 var leftHand = vrTrackers.lefthand;
                 var rightHand = vrTrackers.righthand;
     
-                this.headCube.cframe = new CFrame(head.position.x * 4, head.position.y * 4, head.position.z * 4).multiply(CFAngles(toRadians(head.rotation.x), toRadians(head.rotation.y), 0));
-                this.leftCube.cframe = new CFrame(leftHand.position.x * 4, leftHand.position.y * 4, leftHand.position.z * 4).multiply(CFAngles(toRadians(leftHand.rotation.x), toRadians(leftHand.rotation.y), 0));
-                this.rightCube.cframe = new CFrame(rightHand.position.x * 4, rightHand.position.y * 4, rightHand.position.z * 4).multiply(CFAngles(toRadians(rightHand.rotation.x), toRadians(rightHand.rotation.y), 0));
-    
-                this.headCube.update();
-                this.leftCube.update();
-                this.rightCube.update();
+                this.headCube.position = new Vec3(head.position.x * 4, head.position.y * 4, head.position.z * 4);
+                this.headCube.rotation = new Vec3(head.rotation.x, head.rotation.y, head.rotation.z);
+                
+                this.leftCube.position = new Vec3(leftHand.position.x * 4, leftHand.position.y * 4, leftHand.position.z * 4);
+                this.leftCube.rotation = new Vec3(leftHand.rotation.x, leftHand.rotation.y, leftHand.rotation.z):
+                
+                this.rightCube.position = new Vec3(rightHand.position.x * 4, rightHand.position.y * 4, rightHand.position.z * 4);
+                this.rightCube.rotation = new Vec3(rightHand.rotation.x, rightHand.rotation.y, rightHand.rotation.z);
             }
         });
 
@@ -51,12 +61,20 @@ class VRHandler {
                 this.trackingPlayers = false;
                 this.rendering = false;
 
+                this.headCube.updating = false;
+                this.leftCube.updating = false;
+                this.rightCube.updating = false;
+
                 await new Promise(resolve => setTimeout(resolve, 1000));
-                data = await JSONSender.getChunk(ws, this.headCube.cframe.position);
+                data = await JSONSender.getChunk(ws, this.headCube.position);
 
                 this.loadingWorld = false;
                 this.trackingPlayers = true;
                 this.rendering = true;
+
+                this.headCube.updating = true;
+                this.leftCube.updating = true;
+                this.rightCube.updating = true;
 
                 vrSocket.send(JSON.stringify({
                     blockCoords: data
@@ -66,24 +84,24 @@ class VRHandler {
 
         vrSocket.on("PlaceBlock", async () => {
             if (!this.loadingWorld) {
-                JSONSender.setBlock(ws, this.rightCube.cframe.position, this.pickedBlock);
+                JSONSender.setBlock(ws, this.rightCube.position, this.pickedBlock);
                 vrSocket.send(JSON.stringify({
-                    blockCoords: [[this.pickedBlock], [this.rightCube.cframe.position.floored()]]
+                    blockCoords: [[this.pickedBlock], [this.rightCube.position.floored()]]
                 }));
             }
         })
 
         vrSocket.on("BreakBlock", async () => {
             if (!this.loadingWorld) {
-                JSONSender.setBlock(ws, this.rightCube.cframe.position, "air");
+                JSONSender.setBlock(ws, this.rightCube.position, "air");
                 vrSocket.send(JSON.stringify({
-                    blockCoords: [["minecraft:air"], [this.rightCube.cframe.position.floored()]]
+                    blockCoords: [["minecraft:air"], [this.rightCube.position.floored()]]
                 }));
             }
         })
 
         vrSocket.on("PickBlock", async () => {
-            this.pickedBlock = (await JSONSender.getBlock(ws, this.rightCube.cframe.position.floored()));
+            this.pickedBlock = (await JSONSender.getBlock(ws, this.rightCube.position.floored()));
         })
 
         setInterval(async () => {
